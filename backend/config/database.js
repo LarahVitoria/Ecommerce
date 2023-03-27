ceeef4bdc83a7b38cdc39719bfb1021666286ae3
Lara <larahvitoria15@gmail.com>
2023-03-27 20:33:44 -0300

    Instalando e configurando o next e strap

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.pzwbazw.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ecommerce_bd'),
+        username: env('DATABASE_USERNAME', 'larah'),
+        password: env('DATABASE_PASSWORD', 'pudimD4.'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05fe3e87-6481-4e36-b68d-f179eb11f11c'
+};
