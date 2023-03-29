8b71455a99acb231e25957b9074b5d1ff9bcb9c4
Lara <larahvitoria15@gmail.com>
2023-03-29 16:20:43 -0300

    Configurando o redux e a baseUrl para otimizar o tempo

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux'
 import { GlogalStyle } from '../styles/global-styles'
 import { ThemeProvider } from 'styled-components'
 import '../styles/vendors/index.scss'
 import { theme } from '../styles/theme'
+import { store } from '../store/store'
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
-		<ThemeProvider theme={theme}>
-			<GlogalStyle />
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<GlogalStyle />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</Provider>
 	)
 }
 
